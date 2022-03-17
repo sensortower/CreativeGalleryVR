0c8609cccee59b9bed6ed923ac8dedd6fdd03a43
Samuele Zanella <samuele.zanella@sensortower.com>
2022-03-17 16:11:00 +0100

    Add video entity dynamically

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 async function preloadResource(creative) {
     const res = await fetch(creative.url);
     const blob = await res.blob();
-    const videoUrl = URL.createObjectURL(blob);
-    const videoEntity = document.getElementById('video-element');
+    const videoBlobUrl = URL.createObjectURL(blob);
+
+    const videoEntity = createVideoEntity(creative);
+    const videoDOMElement = createVideoDOMElement(creative, videoBlobUrl);
+
+    // Add the asset to the a-video
+    videoEntity.setAttribute('src', `#${videoDOMElement.id}`);
+
+    // Start playback
+    videoDOMElement.muted = true;
+    await videoDOMElement.play();
+}
+
+function createVideoEntity(creative) {
+    const videoEntity = document.createElement('a-video');
+    const videoEntityId = `video-entity-${creative.id}`
+    videoEntity.setAttribute('id', videoEntityId);
+    videoEntity.setAttribute('position', '0 3 -4');
+    videoEntity.setAttribute('rotation', '0 0 0');
+    videoEntity.setAttribute('width', '4');
+    videoEntity.setAttribute('height', '2');
+    videoEntity.setAttribute('animation', 'property: components.material.material.opacity; from: 0; to: 1; dur: 750; easing: easeOutQuad');
+
+    document.getElementById('my-scene').appendChild(videoEntity);
+
+    return videoEntity;
+}
+
+function createVideoDOMElement(creative, videoBlobUrl) {
     const videoDOMElement = document.createElement('video');
     const videoDOMElementId = `dynamic-video-${creative.id}`
-    videoDOMElement.setAttribute('id', videoDOMElementId); // Create a unique id for asset
-    videoDOMElement.setAttribute('src', videoUrl);
+    videoDOMElement.setAttribute('id', videoDOMElementId);
+    videoDOMElement.setAttribute('src', videoBlobUrl);
     videoDOMElement.setAttribute('style', 'display: none;')
     videoDOMElement.setAttribute('loop', 'true')
     // Append the new video to the a-assets, where a-assets id="assets-id"
     document.getElementById('video-container').appendChild(videoDOMElement);
 
-    // Add the asset to the a-video
-    videoEntity.setAttribute('src', `#${videoDOMElementId}`);
-    // Start playback
-    videoDOMElement.muted = true;
-    await videoDOMElement.play();
+    return videoDOMElement;
 }
 
 async function fetchCreatives() {
